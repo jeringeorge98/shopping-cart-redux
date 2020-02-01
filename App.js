15e36cce1e85d09fb5aa188fac99bc25dfcc6a03
Jerin George <jerringeorge439@gmail.com>
2020-02-02 02:40:02 +0530

    changes

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {View,Text} from 'react-native'
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>hello</Text>
+      </View>
+    )
+  }
+}
+
+export default App
